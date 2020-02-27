735ae635345c6a8be667232c9b5e807f801d0f09
PuniCharana <punicharana@gmail.com>
2020-02-27 12:11:52 +0530

    remove spec files

--- src/app/modules/home/demo/demo.component.ts
+++ src/app/modules/home/demo/demo.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-demo',
-  templateUrl: './demo.component.html',
+  template: `<router-outlet></router-outlet>`,
   styleUrls: ['./demo.component.scss']
 })
 export class DemoComponent implements OnInit {
